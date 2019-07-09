bf91a3a316c6ebc7dc5792394a5c88369600f3cb
wangzhiqiang <1071683940@qq.com>
2019-07-09 20:36:33 +0800

    chore: commitlint配置修改

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'css', 'docs', 'style', 'pref', 'config', 'revert']
-    ],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never']
-  }
-}
+module.exports = {extends: ['@commitlint/config-conventional']}
