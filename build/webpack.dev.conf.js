38c11bafb4fb3e3490a18bd484df20095906081c
wangzhiqiang <1071683940@qq.com>
2019-08-01 17:34:15 +0800

    chore: 修改部分配置
    # head: <type>: <subject>
    # - type:
    # - feat：添加新功能。
    # - fix：修复 bug。
    # - style：代码格式。
    # - perf：优化相关。
    # - revert：分支回溯。
    # - chore：构建配置相关。
    # - docs：文档相关。
    # - build: 打包相关
    
    # - subject: start with verb (such as 'change'), 50-character line

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -4,20 +4,18 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./base.conf')
 const copyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
 const config = {
-  host: '0.0.0.0',
+  host: 'localhost',
   port: 9000
 }
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
   entry: {
     main: './example/main.js',
-    vendor: ['vue', 'vue-router', 'axios']
+    vendors: ['vue', 'vue-router']
   },
   output: {
     path: path.join(__dirname, './../dist'),
@@ -49,14 +47,6 @@ module.exports = merge(baseWebpackConfig, {
           { loader: 'css-loader', options: { importLoaders: 2 } },
           'postcss-loader',
           {
-            loader: 'postcss-loader',
-            options: {
-              config: {
-                path: 'postcss.config.js' // 根目录创建此文件
-              }
-            }
-          },
-          {
             loader: 'less-loader' // compiles Less to CSS
           }
         ]
@@ -81,26 +71,20 @@ module.exports = merge(baseWebpackConfig, {
   optimization: {
     splitChunks: {
       chunks: 'all',
-      minSize: 30000,
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/,
           priority: -10
         },
         default: {
-          minChunks: 1,
+          minChunks: 2,
           priority: -20,
           reuseExistingChunk: true
         }
       }
-    },
-    runtimeChunk: {
-      name: entrypoint => `runtime~${entrypoint.name}`
     }
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new webpack.HashedModuleIdsPlugin(),
     new copyWebpackPlugin([
       {
         from: path.resolve(__dirname + './../static'), // 打包的静态资源目录地址
