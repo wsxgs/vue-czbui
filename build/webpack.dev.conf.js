c23b98a7ac78dbaa3a1f138a8984ddbeea01983b
wangzhiqiang <1071683940@qq.com>
2019-08-09 10:29:08 +0800

    chore: workbox配置修改
    # head: <type>: <subject>
    # - type:
    # - feat：添加新功能。
    # - fix：修复 bug。
    # - style：代码格式。
    # - perf：优化相关。
    # - revert：分支回溯。
    # - chore：构建配置相关。
    # - docs：文档相关。
    # - build: 打包相关
    
    # - subject: start with verb (such as 'change'), 50-character line

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -107,8 +107,12 @@ module.exports = merge(baseWebpackConfig, {
       template: 'index.html'
     }),
     new workboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
+      cacheId: 'webpack-pwa', // 设置前缀
+      skipWaiting: true, // 强制等待中的 Service Worker 被激活
+      clientsClaim: true, // Service Worker 被激活后使其立即获得页面控制权
+      swDest: 'service-wroker.js', // 输出 Service worker 文件
+      globPatterns: ['**/*.{html,js,css,png.jpg}'], // 匹配的文件
+      globIgnores: ['service-wroker.js'], // 忽略的文件
       runtimeCaching: [
         // 配置路由请求缓存
         {
--- example/service-worker.js
+++ example/service-worker.js
@@ -1,10 +1,12 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker
-    .register('/service-worker.js')
-    .then(registration => {
-      console.log('serviceWorker register success')
-    })
-    .catch(e => {
-      console.error('serviceWorker register failed')
-    })
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('serviceWorker register success')
+      })
+      .catch(e => {
+        console.error('serviceWorker register failed')
+      })
+  })
 }
